f897a32a29a47d54b43dc618a16a227e701587c7
Souad <souadomerarabii@gmail.com>
2023-07-13 14:40:29 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - Entry point of the program
- *
- * Description: This program prints the size of various computer types,
- * including char, int, long int, long long int, and float.
- *
- * Return: 0 (success)
- *         This function returns an integer value to indicate the execution status.
- *         The value 0 is returned to indicate successful execution of the program.
+ * main - A program that prints the size of various computer types
+ * Return 0 (Success)
  */
-int main(void)
+ int main(void)
 {
         char a;
         int b;
@@ -18,11 +11,10 @@ int main(void)
         long long int d;
         float f;
 
-        printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-        printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-        printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-
-        return 0;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
