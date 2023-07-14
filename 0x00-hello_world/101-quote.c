c5799892dc53153bd041cc8bf87cd0ad602e9ed5
Souad <souadomerarabii@gmail.com>
2023-07-14 11:56:46 -0700

    teemo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error 
- * Return 1 (Success)
- */
+*main - Entry point
+*Description: writes required string not using puts or printf
+*using unistd
+*Return: Always success 1
+*/
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
