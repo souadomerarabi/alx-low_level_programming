24dc4db4a56e0df5c007dd6724ecc740bce44397
Souad <souadomerarabii@gmail.com>
2023-07-16 14:55:29 -0700

    neww code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the numbers from 0 to 9 using only putchar function
+ *
+ * Return: 0 if the program completes successfully
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
+
