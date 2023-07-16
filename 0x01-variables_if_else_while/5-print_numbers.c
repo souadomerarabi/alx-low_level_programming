9e768a16add470df9f42f45c7c9c5246bd51307a
Souad <souadomerarabii@gmail.com>
2023-07-16 14:49:27 -0700

    neww code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the alphabet in lowercase using only putchar function
+ *
+ * Return: 0 if the program completes successfully
+ */
+int main(void)
+{
+	char ch;
+
+	for (int n=0; n<10; n++)
+	{
+		printf("%d", n);
+	}
+	printf('\n');
+
+	return (0);
+}
+
