cee2dd7b39700ae40eee72724f1a7cb99b79fac0
Souad <souadomerarabii@gmail.com>
2023-07-16 14:40:03 -0700

    neww code

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,14 +11,14 @@
  */
 int main(void)
 {
-    char ch;
+	char ch;
 
-    for(ch = 'a'; ch <= 'z'; ch++)
-    {
-        putchar(ch);
-    }
-    putchar('\n');
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
