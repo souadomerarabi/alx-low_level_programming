aec26244e5274c5c8a49f1080ec9823d06fa3716
Souad <souadomerarabii@gmail.com>
2023-07-19 15:01:50 -0700

    teemo

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - adds two integers
+* @a: integer to be added
+* @b: integer to be added
+*
+* Return: value of addition
+*/
+int add(int a, int b)
+{
+int r;
+r = a + b;
+return (r);
+}
