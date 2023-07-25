6bcd755fa802eb328e0dd6cf44203476b48779b7
Souad <souadomerarabii@gmail.com>
2023-07-25 09:18:45 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: FizzBuzz
+ * using standard library
+ * Return: Always success 0
+ */
+int main(void)
+{
+int i, a, b;
+for (i = 1; i <= 100; i++)
+{
+if (i != 100)
+{
+a = i % 3;
+b = i % 5;
+if (a != 0 && b != 0)
+printf("%d ", i);
+else if (a == 0 && b != 0)
+printf("Fizz ");
+else if (a != 0 && b == 0)
+printf("Buzz ");
+else
+printf("FizzBuzz ");
+}
+else
+printf("Buzz\n");
+}
+return (0);
+}
