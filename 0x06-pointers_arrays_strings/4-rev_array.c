4beb8930ecfcdbdeb737caba7784cb8bd0f669de
Souad <souadomerarabii@gmail.com>
2023-07-26 17:36:48 -0700

    stract

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,27 @@
 #include "holberton.h"
+
 /**
- * reverse_array - prints reverse array.
- * @a: array to be compared.
- * @n: size of array.
- * Return: reversed array.
+ * reverse_array - Write a function that reverses the content of an array of
+ * integers.
+ *
+ * @a: This is the input array
+ * @n: This is the positions have the array
+ *
  */
 void reverse_array(int *a, int n)
 {
-	int swap, begin, end;
+	int i, last;
 
-	begin = 0;
-	end = n - 1;
-	while (begin < end)
+	last = n - 1;
+	for (i = 0; i < n / 2; i++)
 	{
-		swap = *(a + begin);
-		*(a + begin) = *(a + end);
-		*(a + end) = swap;
-		begin++;
-		end--;
+		int start, end;
+
+		start = a[i];
+		end = a[last];
+		a[i] = end;
+		a[last] = start;
+		last--;
 	}
 }
+
