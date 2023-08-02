bddb65b692f71fbddfec5edb9410c303164c7b03
Souad <souadomerarabii@gmail.com>
2023-08-01 19:12:06 -0700

    susu7

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,25 @@
 #include "main.h"
 /**
- * *_strchr - locates
- * @s: array
- * @c: to be located
- * Return: dest
+ * _strchr - prints from the first occurrence of a char.
+ * @s: source string
+ * @c: tested char
+ *
+ * Return: new string.
  */
 char *_strchr(char *s, char c)
 {
-int test = 0;
-for (; test == 0 && *s != '\0'; s++)
-{
-if (*s == c)
-test = 1;
-}
-if (test == 1)
-return (s - 1);
-else
-return ('\0');
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+
+		}
+		else
+			s++;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return (0);
 }
