293fe36f17a333d08800c423ed82eeb4cd69fe6a
Souad <souadomerarabii@gmail.com>
2023-08-03 16:19:00 -0700

    susu8

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen_recursion - finds the length
+ * @s: to be counted
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
+}
