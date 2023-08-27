342cc10c234c33dc7206708a0eb5a3341552a7a1
Souad <souadomerarabii@gmail.com>
2023-08-26 18:42:41 -0700

    If you spend too much time thinking about a thing, you'll never get it done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_POINTERS_H
+#define FUNCTIONS_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif
+
