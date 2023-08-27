57a9392724a88ef2aef86e73ef871b87267ca2f2
Souad <souadomerarabii@gmail.com>
2023-08-26 17:05:44 -0700

    D

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints
+ *@name: name
+ *@f: function
+ * Return: null
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+f(name);
+return;
+}
