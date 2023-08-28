ef9d53b7a65cf1c97cf16b2d4bff132a8096ad5d
Souad <souadomerarabii@gmail.com>
2023-08-28 14:44:42 -0700

    noncat

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,27 +1,37 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * array_range -  an array of integers
- * @min: minimum value in array
- * @max: maaximum value in array
- * Return: integer array
+ * array_range - function that creates an array of integers.
+ * @min: minimum integer in array.
+ * @max: maximum integer in array.
+ *
+ * The array created should contain all the values from min (included),
+ * to max (included), ordered from min to max.
+ * If min > max, return NULL.
+ * If malloc fails, return NULL.
+ *
+ * To avoid a memory leak, the returned pointer must be deallocated,
+ * with free() or realloc() but realloc is forbidden here..
+ *
+ * Return: A pointer to the newly created array (success).
  */
+
 int *array_range(int min, int max)
 {
-	int i, size = (max - min) + 1;
 	int *p;
+	int i, size;
+
+	size = (max - min) + 1;
 
 	if (min > max)
 		return (NULL);
 
-	p = (int *) malloc(sizeof(int) * size);
+	p = malloc(size * sizeof(*p));
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-	{
-		p[i] = min;
-		min++;
-	}
+	for (i = 0; i < size && min <= max; i++, min++)
+		*(p + i) = min; /* p[i] */
 	return (p);
 }
