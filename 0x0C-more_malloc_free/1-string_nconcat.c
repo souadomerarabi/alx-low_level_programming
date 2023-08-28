90e4dd8a92fd75618b09fd289499ab96c22c7ce3
Souad <souadomerarabii@gmail.com>
2023-08-28 14:38:10 -0700

    noncat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,48 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: destination string
- * @s2: source string
- * @n: number of characters to be copied
- * Return: copied
+ * string_nconcat - function that concatenates two strings.
+ * @s1: pointer to the null-terminated byte string to append to.
+ * @s2: pointer to the null-terminated byte string to copy from.
+ * @n: number of bytes from s2 to concatenate.
+ *
+ * If n is greater or equal to the length of s2 then use the entire string s2.
+ * If NULL is passed, treat it as an empty string
+ *
+ * Return: NULL (fail) or a pointer to newly allocated space in memory
+ * which contains the contents of s1, followed by the contents of s2,
+ * and null-terminated (Success).
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_len = 0, s2_len = 0, i;
-	char *s;
+	char *dupStr;
+	unsigned int j;
+	int i;
 
-	/* if NULL is passed, treat as an empty string */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
 
-	/* While at index 0 */
-	while (s1[s1_len] != '\0')
-		s1_len++;
-	while (s2[s2_len] != '\0')
-		s2_len++;
-
-	if (n >= s2_len)
-		n = s2_len;
-
-	/* Memory allocation */
-	s = malloc(sizeof(char) * n + s1_len + 1);
-	if (s == NULL)
+	dupStr = malloc(sizeof(char) * (i + n + 1));
+	if (dupStr == NULL)
 		return (NULL);
-
-	for (i = 0; i < s1_len; i++)
-		s[i] = s1[i];
-
-	for (i = 0; i < n; i++)
-		s[i + s1_len] = s2[i];
-
-	s[i + s1_len] = '\0';
-
-	return (s);
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		dupStr[i] = s1[i];
+		i++;
+	}
+	while (j < n && s2[j] != '\0')
+	{
+		dupStr[i] = s2[j];
+		i++, j++;
+	}
+	dupStr[i] = '\0';
+	return (dupStr);
 }
